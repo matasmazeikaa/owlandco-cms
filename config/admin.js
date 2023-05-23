7acc1f90f5803801067e0bcb262cd48a91f88aeb
matasmazeikaa <matasmazeika@gmail.com>
2023-05-23 20:13:23 +0300

    setup admin/api urls

--- config/admin.js
+++ config/admin.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+  url: `${env("PUBLIC_URL")}/owlandco/admin`,
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
